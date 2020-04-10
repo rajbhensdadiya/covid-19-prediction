aa1ce920b8c28f7e615a84ed5c0e524561caf8f1
Raj Bhensdadiya <rkumar79441@gmail.com>
2020-04-10 09:35:30 +0530

    commit

---
+++ app.py
@@ -0,0 +1,68 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+from datetime import date
+import pickle
+import re
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb')) #confirm
+model1 = pickle.load(open('model1.pkl', 'rb')) #death
+model2 = pickle.load(open('model2.pkl', 'rb')) #recover
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    f_date=date(2020,1,22)
+    features = [str(x) for x in request.form.values()]
+    features = features[0]
+    if re.match(r"[\d]{1,2}/[\d]{1,2}/[\d]{4}", features):
+        features = features.split("/")
+        
+        if(int(features[0])<=31):
+            pass
+        else:
+            return render_template('index.html',prediction_text="Date should be upto 31")
+        
+        if(int(features[1])<=4):
+            pass
+        else:
+            return render_template('index.html',prediction_text="Month should be upto April")  
+
+        if(int(features[2])==2020):
+            pass
+        else:
+            return render_template('index.html',prediction_text="Year should be 2020")
+        pass
+        
+    else:
+        return render_template('index.html',prediction_text="Date should be in format dd/mm/yyyy")
+    
+
+    l_date=date(int(features[2]),int(features[1]),int(features[0]))
+    delta = l_date - f_date
+    prediction = model.predict([[delta.days]])
+    prediction1 = model1.predict([[delta.days]])
+    prediction2 = model2.predict([[delta.days]])
+
+    return render_template('index.html', prediction_text="Total number of Confirmed Cases: {}".format(int(prediction)),prediction_text1="Total number of Death: {}".format(int(prediction1)),prediction_text2="Total number of Recover Patients: {}".format(int(prediction2)))
+@app.route('/visual')
+def visual():
+    return render_template('visualize.html')
+"""@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)"""
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ predict.py
@@ -0,0 +1,72 @@
+import numpy as np 
+import pandas as pd
+import datetime
+import pickle
+
+
+confirmed_data = pd.read_csv('time_series_19-covid-Confirmed.csv')
+death_data = pd.read_csv('time_series_19-covid-Deaths.csv')
+recovered_data = pd.read_csv('time_series_19-covid-Recovered.csv')
+
+confirmed = confirmed_data.iloc[:,4:] 
+death = death_data.iloc[:,4:]
+recovered = recovered_data.iloc[:,4:]
+
+
+dates = confirmed.keys()
+world_cases = []
+world_deaths = []
+world_recovered = []
+
+for i in dates:
+    confirmed_sum = confirmed[i].sum()
+    death_sum = death[i].sum()
+    recovered_sum = recovered[i].sum()
+    # confirmed, deaths, recovered, and active
+    world_cases.append(confirmed_sum)
+    world_deaths.append(death_sum)
+    world_recovered.append(recovered_sum)
+    
+
+
+
+days_since_1_22 = np.array([i for i in range(len(dates))]).reshape(-1, 1)
+world_cases = np.array(world_cases).reshape(-1, 1)
+world_deaths = np.array(world_deaths).reshape(-1, 1)
+world_recovered = np.array(world_recovered).reshape(-1, 1)
+
+
+
+
+days_in_future = 10
+c = len(dates)
+future_forcast = np.array([i+c for i in range(days_in_future)]).reshape(-1, 1)
+#adjusted_dates = future_forcast[:-10]
+
+
+"""start = '1/22/2020'
+start_date = datetime.datetime.strptime(start, '%m/%d/%Y')
+future_forcast_dates = []
+for i in range(len(future_forcast)):
+    future_forcast_dates.append((start_date + datetime.timedelta(days=i)).strftime('%m/%d/%Y'))"""
+
+"""from sklearn.model_selection import train_test_split
+X_train_confirmed, X_test_confirmed, y_train_confirmed, y_test_confirmed = train_test_split(days_since_1_22, world_cases, test_size=0.15, shuffle=False)""" 
+
+from sklearn.svm import SVR
+regressor = SVR(shrinking=True, kernel='poly',gamma=0.01, epsilon=1,degree=5, C=0.1)
+regressor.fit(days_since_1_22,world_cases)
+y_pred_cases = regressor.predict(future_forcast)
+regressor1 = SVR(shrinking=True, kernel='poly',gamma=0.01, epsilon=1,degree=5, C=0.1)
+regressor1.fit(days_since_1_22,world_deaths)
+y_pred_deaths = regressor1.predict(future_forcast)
+regressor2 = SVR(shrinking=True, kernel='poly',gamma=0.01, epsilon=1,degree=5, C=0.1)
+regressor2.fit(days_since_1_22,world_recovered)
+y_pred_recovered = regressor2.predict(future_forcast)
+
+filename = 'model.pkl'
+pickle.dump(regressor, open(filename, 'wb'))
+filename = 'model1.pkl'
+pickle.dump(regressor1, open(filename, 'wb'))
+filename = 'model2.pkl'
+pickle.dump(regressor2, open(filename, 'wb'))
